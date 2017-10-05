67145eca04aec9aa2b3b225d11aaeb0d7702905a
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-10-05 09:39:06 -0700

    updating logic for time arrival

--- javascript/logic.js
+++ javascript/logic.js
@@ -6,22 +6,25 @@ $("#submit-btn").on("click", function(event) {
 
   event.preventDefault();
 
-  var trainName = $("#trainName").val().trim();
-  var destination = $("#destination").val().trim();
-  var firstTrainToLeave = parseInt(moment($("#trainArriveTime").val().trim(),"HH:mm").format("HH:mm"));
-  var frequency = $("#frequencyOfTrain").val().trim();
+  var trainNameVal = $("#trainName").val().trim();
+  var destinationVal = $("#destination").val().trim();
+  var firstTrainToLeaveVal = $("#trainArriveTime").val().trim();
+  // var firstTrainToLeaveSplit = firstTrainToLeave;
+  var frequencyVal = $("#frequencyOfTrain").val().trim();
+
 
     database.ref().push({
-      trainName: trainName,
-      destination : destination,
-      frequency: frequency,
-      firstTrainToLeave : firstTrainToLeave
+      trainNameKey: trainNameVal,
+      destinationKey : destinationVal,
+      frequencyKey: frequencyVal,
+      firstTrainToLeaveKey : firstTrainToLeaveVal
     })
 
-  console.log(trainName);
-  console.log(destination);
-  console.log(frequency);
-  console.log(typeof(firstTrainToLeave));
+  console.log("Name of Train " + trainNameVal);
+  console.log("The destination "+ destinationVal);
+  console.log("The Frequency " + frequencyVal);
+  console.log("When the first train arrives "+firstTrainToLeaveVal);
+  // console.log(firstTrainToLeaveSplit);
 
   $("#trainName").val("");
   $("#destination").val("");
@@ -29,55 +32,55 @@ $("#submit-btn").on("click", function(event) {
   $("#frequencyOfTrain").val("");
 
 
-})
+});
 
-database.ref().on("child_added",function(childSnapshot, prevChildKey){
+database.ref().on("child_added",function(childSnapshot, prevChildKey) {
 
-      // Getting the frequency of each train
-      var trainFrequency = parseInt(childSnapshot.val().frequency);
+  // Getting the frequency of each train
+  var trainFrequency = parseInt(childSnapshot.val().frequencyKey);
 
-      console.log(trainFrequency);
+  console.log("Name of Train " + trainFrequency +" from database");
 
-      // Retriving the start time of each train
-      var firstTrainLeaving =parseInt(childSnapshot.val().firstTrainToLeave);
+  // Retriving the start time of each train
+  var firstTrainLeaving =parseInt(childSnapshot.val().firstTrainToLeaveKey);
 
-      console.log(firstTrainLeaving);
+  console.log("When the first train arrives" + firstTrainLeaving + "from database");
 
-      // Converting the first train to leave
-      var firstTrainTimeConvert = moment(firstTrainLeaving, "HH:mm").subtract(1, "years");
-      console.log(firstTrainTimeConvert);
+  // Converting the first train to leave
+  var firstTrainTimeConvert = moment(firstTrainLeaving, "HH:mm").subtract(1, "years");
+  console.log("The converts the time "+ firstTrainTimeConvert);
 
-      // Current time
-      var currentTime = moment();
-      console.log("Current time:" + moment(currentTime).format("HH:mm"));
+  // Current time
+  var currentTime = moment();
+  console.log("Current time:" + moment(currentTime).format("HH:mm"));
 
-      // Difference in time
-      var differenceInTime = moment().diff(moment(firstTrainTimeConvert), "minutes");
-      console.log(differenceInTime);
+  // Difference in time
+  var differenceInTime = moment().diff(moment(firstTrainTimeConvert), "minutes");
+  console.log(differenceInTime);
 
-      // Time apart
-      var timeRemainder = differenceInTime % trainFrequency;
-      console.log(timeRemainder);
+  // Time apart
+  var timeRemainder = differenceInTime % trainFrequency;
+  console.log(timeRemainder);
 
-      var timeTilNextTrain = trainFrequency - timeRemainder;
-        console.log("Minutes till train" + timeTilNextTrain);
+  var timeTilNextTrain = trainFrequency - timeRemainder;
+    console.log("Minutes till train" + timeTilNextTrain);
 
-      var nextTrainArriving = moment().add(timeTilNextTrain, "Minutes");
-      console.log("arrival time of next train " + moment(nextTrainArriving).format("HH:mm"));
+  var nextTrainArriving = moment().add(timeTilNextTrain, "Minutes");
+  console.log("arrival time of next train " + moment(nextTrainArriving).format("HH:mm"));
 
 
-      var row = $("<tr>");
-      row.append($("<td>").text(childSnapshot.val().trainName));
-      row.append($("<td>").text(childSnapshot.val().destination));
-      row.append($("<td>").text(childSnapshot.val().frequency));
-      row.append($("<td>").text(moment(nextTrainArriving).format("HH:mm")));
-      row.append($("<td>").text(timeTilNextTrain));
-      $("tbody").append(row);
+  var row = $("<tr>");
+  row.append($("<td>").text(childSnapshot.val().trainNameKey));
+  row.append($("<td>").text(childSnapshot.val().destinationKey));
+  row.append($("<td>").text(childSnapshot.val().frequencyKey));
+  row.append($("<td>").text(moment(nextTrainArriving).format("HH:mm")));
+  row.append($("<td>").text(timeTilNextTrain));
+  $("tbody").append(row);
 
 
   // two math functions looking at current time to determine the train schedule and calculate next arriving
 
-},function(errorObject){
+},function(errorObject) {
   ("there is an error"+ errorObject.val())
 });
 
